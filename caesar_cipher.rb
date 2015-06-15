4ef7060d264a7a42fbf466418cc1935bc7cc4fb7
Raymond E Cochrane <raymondecochrane@gmail.com>
2015-06-15 14:31:07 +0100

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,22 @@
+def caesar_cipher(input, n)
+
+	char_nums = [] 
+	cypher_nums	= [] 
+
+	chars = input.downcase.split("")
+	chars.each {|char| char_nums<<char.ord} 
+	char_nums.each do |char_num| 
+			if char_num > 96 
+			char_num += n
+			end	
+			if char_num > 122
+			char_num -= 26
+			end
+		cypher_nums<<char_num.chr
+	end
+	 
+	puts cypher_nums.join.capitalize!
+	
+end
+
+puts caesar_cipher("What a string!", 5)
