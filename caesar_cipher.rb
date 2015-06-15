fab618395f573c1540541400aa992ea51f55066a
Raycotek <Raycotek@users.noreply.github.com>
2015-06-15 22:13:07 +0100

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,17 @@
 def caesar_cipher(input, n)
-
+	
 	char_nums = [] 
 	cypher_nums = [] 
 
 	chars = input.downcase.split("")
 	chars.each {|char| char_nums<<char.ord} 
 	char_nums.each do |char_num| 
-			if char_num > 96 
-			char_num += n
-			end	
-			if char_num > 122
-			char_num -= 26
-			end
+		if char_num > 96 
+		char_num += n
+		end	
+		if char_num > 122
+		char_num -= 26
+		end
 		cypher_nums<<char_num.chr
 	end
 	 
